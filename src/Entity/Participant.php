251c6e72a731e5bf7aedfd8a937b3ca1b7ffb4e3
mrFarno <felicienarnault@gmail.com>
2019-08-27 17:09:21 +0200

    FOSuser init

---
+++ src/Entity/Participant.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as FosUser;
+
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ParticipantRepository")
+ */
+class Participant extends FosUser
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
