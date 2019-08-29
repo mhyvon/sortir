d211d11f46e00f5cf115d482c4445931c85c0724
mrFarno <felicienarnault@gmail.com>
2019-08-29 11:54:06 +0200

    submit

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -7,6 +7,7 @@ use App\Entity\Participant;
 use App\Entity\Site;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -22,7 +23,7 @@ class RegistrationType extends AbstractType
                 'class'=>Site::class,
         ])
             ->add('actif')
-            ->add('submit');
+            ->add('submit', SubmitType::class);
     }
 
     public function getParent()
