89b906d7039cbc8734fd63718e25df5b886d1e56
Amr585 <anthony.marhadour2019@campus-eni.fr>
2019-09-02 14:39:48 +0200

    participants

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
@@ -23,7 +24,7 @@ class SortieType extends AbstractType
         $builder
             ->add('nom')
             ->add('debut')
-            ->add('duree', TimeType::class,[
+            ->add('duree', IntegerType::class,[
                 'attr'=>[
                     'class'=>'duree',
                 ]
